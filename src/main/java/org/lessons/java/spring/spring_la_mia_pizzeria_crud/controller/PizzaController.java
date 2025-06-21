ce36b39a478acdc9b49eafbf4959d9988b0bed28
alessandroschiano98 <schianoski98@gmail.com>
2025-06-21 11:41:19 +0200

    aggiunta: CREATE

--- src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -5,9 +5,14 @@ import org.lessons.java.spring.spring_la_mia_pizzeria_crud.repository.PizzaRepos
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -33,5 +38,22 @@ public class PizzaController {
         model.addAttribute("pizza", pizza);
         return "pizze/show";
     }
+
+    @GetMapping("/create")
+public String create(Model model){
+    model.addAttribute("pizza", new Pizza());
+    return "pizze/create";
+}
+
+@PostMapping("/create")
+public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model){
+
+    if (bindingResult.hasErrors()){
+        return "pizze/create";
+    }
+
+    pizzaRepository.save(formPizza);
+    return "redirect:/pizze";
+}
 
 }
--- src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
+++ src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/repository/PizzaRepository.java
@@ -3,9 +3,16 @@ package org.lessons.java.spring.spring_la_mia_pizzeria_crud.repository;
 import org.lessons.java.spring.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
+
 
 @Repository
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
+    List<Pizza> findByNomeContainingIgnoreCase(String nome);
+
+    List<Pizza> findByDescrizioneContainingIgnoreCase(String descrizione);
+
 }
+
 
