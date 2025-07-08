28f6780f5ce2fa6e801c1a525100e33ec4073b6a
alessandroschiano98 <schianoski98@gmail.com>
2025-07-08 18:52:33 +0200

    fix: errore

--- src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/LaMiaPizzeriaApplication.java
+++ src/main/java/org/lessons/java/spring/spring_la_mia_pizzeria_crud/LaMiaPizzeriaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLaMiaPizzeriaCrudApplication {
+public class LaMiaPizzeriaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLaMiaPizzeriaCrudApplication.class, args);
+		SpringApplication.run(LaMiaPizzeriaApplication.class, args);
 	}
 
 }
